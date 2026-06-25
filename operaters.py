274e078200563bcd0e193c36ebe8d575c94feb25
Nisha sonkusre <nishasonkusre07@gmail.com>
2026-06-25 16:36:20 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,216 @@
+# python operaters , variable and Data types
+
+'''
+1.Arithmetic Operater
+2.Assignment Operater
+3.Comparision Operater
+4.Logical Operater
+5.Bitwise Operater
+6.Type conversion
+'''
+
+a = 10
+b = 20
+
+print('addition:',a+b)
+print('substraction:',a-b)
+print('multipliction:',a*b)
+print('division:',a/b)
+print('modulus:',a%b)
+print('exponentiation:',a**b)
+print('floor division:',a//b)
+
+# Assignment Operator
+
+x = 5 #simple assignment
+
+y = 3
+
+x +=y #x=x+y
+
+print(x)
+print(y)
+
+x -= y
+
+print(x)
+print(y)
+
+x /= y
+
+print(x)
+print(y)
+
+x %= y
+
+print(x)
+print(y)
+
+x **= y
+
+print(x)
+print(y)
+
+x //=y
+
+print(x)
+print(y)
+
+#comparisionoperator
+
+x = 10
+y = 10
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x <= y)
+print(x > y)
+print(x >= y)
+
+# Logical Operator (and,or,not)
+
+x = True
+y = False
+z = False
+
+print(x and y)
+print(y and z)
+print(x or y)
+print(y or z)
+print(not x)
+print(not y)
+
+# Type conversion
+
+'''
+
+int()
+float()
+str()
+tuple()
+list()
+set()
+dict()
+
+'''
+
+num_str = "123"
+print(type(num_str))
+num_int = int(num_str)
+print(type(num_int))
+print(num_int+7)
+
+# Multiple assignment
+
+x,y,z = 10,20,30
+
+print(x,y,z)
+
+a = 20
+b = 20
+
+print(id(a))
+print(id(b))      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
